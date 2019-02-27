ea4dfa090f349621a9241b3621b1f91a3535c078
hsldymq <hsldymq@qq.com>
2019-02-27 21:09:40 +0800

    增加方法,移除所有信号处理器

--- src/Traits/SignalTrait.php
+++ src/Traits/SignalTrait.php
@@ -9,13 +9,33 @@ use React\EventLoop\LoopInterface;
  */
 trait SignalTrait
 {
+    private $signalHandlers = [];
+
     public function addSignalHandler(int $sig, callable $handler)
     {
         $this->loop->addSignal($sig, $handler);
+
+        $this->signalHandlers[$sig][] = $handler;
     }
 
     public function removeSignalHandler(int $sig, callable $handler)
     {
         $this->loop->removeSignal($sig, $handler);
+
+        foreach (($this->signalHandlers[$sig] ?? []) as $idx => $h) {
+            if ($h === $handler) {
+                unset($this->signalHandlers[$sig][$idx]);
+            }
+        }
+    }
+
+    public function removeAllSignalHandlers()
+    {
+        foreach ($this->signalHandlers as $sig => $handlers) {
+            foreach ($handlers as $idx => $eachHandler) {
+                $this->loop->removeSignal($sig, $eachHandler);
+                unset($this->signalHandlers[$sig][$idx]);
+            }
+        }
     }
 }
