b5a45bbbb2dc4eebee9ccda3717a29efeaf936a1
hsldymq <hsldymq@qq.com>
2019-03-02 01:30:57 +0800

    异常增加参数

--- src/Exception/WorkerNotExistException.php
+++ src/Exception/WorkerNotExistException.php
@@ -2,6 +2,21 @@
 
 namespace Archman\Whisper\Exception;
 
+use Throwable;
+
 class WorkerNotExistException extends \Exception
 {
+    private $workerID;
+
+    public function __construct(string $workerID, string $message = "", int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        $this->workerID = $workerID;
+    }
+
+    public function getWorkerID(): string
+    {
+        return $this->workerID;
+    }
 }
