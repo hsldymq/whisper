087f235b19034521cafd620dda2e6fa0995c5e67
hsldymq <hsldymq@qq.com>
2019-09-11 20:48:25 +0800

    Update SignalTrait.php

--- src/Traits/SignalTrait.php
+++ src/Traits/SignalTrait.php
@@ -13,6 +13,12 @@ trait SignalTrait
 {
     private $signalHandlers = [];
 
+    /**
+     * 添加信号处理器.
+     *
+     * @param int $sig
+     * @param callable $handler
+     */
     public function addSignalHandler(int $sig, callable $handler)
     {
         $this->getEventLoop()->addSignal($sig, $handler);
@@ -20,12 +26,18 @@ trait SignalTrait
         $this->signalHandlers[$sig][] = $handler;
     }
 
-    public function removeSignalHandler(int $sig, callable $handler)
+    /**
+     * 移除信号处理器.
+     *
+     * @param int $sig
+     * @param callable|null $handler 如果不传则移除该信号下的所有处理器
+     */
+    public function removeSignalHandler(int $sig, callable $handler = null)
     {
         $this->getEventLoop()->removeSignal($sig, $handler);
 
         foreach (($this->signalHandlers[$sig] ?? []) as $idx => $h) {
-            if ($h === $handler) {
+            if ($handler === null || $h === $handler) {
                 unset($this->signalHandlers[$sig][$idx]);
             }
         }
