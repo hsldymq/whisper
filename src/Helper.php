b5126b9147d3808244c20d62e3ecc1e26951f050
hsldymq <hsldymq@qq.com>
2022-04-26 03:28:39 +0800

    refactor: uuid generator

--- src/Helper.php
+++ src/Helper.php
@@ -8,20 +8,11 @@ class Helper
 {
     public static function uuid(): string
     {
-        try {
-            $uuid = bin2hex(random_bytes(18));
-            $uuid[8] = $uuid[13] = $uuid[18] = $uuid[23] = '-';
-            $uuid[14] = '4';
-            $uuid[19] = dechex(hexdec($uuid[19]) & 3 | 8);
+        $uuid = bin2hex(random_bytes(18));
+        $uuid[8] = $uuid[13] = $uuid[18] = $uuid[23] = '-';
+        $uuid[14] = '4';
+        $uuid[19] = dechex(hexdec($uuid[19]) & 3 | 8);
 
-            return $uuid;
-        } catch (\Throwable $e) {
-            return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-                mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff),
-                mt_rand(0, 0x0fff) | 0x4000,
-                mt_rand(0, 0x3fff) | 0x8000,
-                mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
-            );
-        }
+        return $uuid;
     }
 }
